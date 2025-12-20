a46726be10d6a1db00261c5b340f059b5fb4cb51
MuchHat1955 <gigelavram@gmail.com>
2025-12-19 18:58:21 -0800

    copyv71

---
+++ openrb_dynamixel_v71/utils.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <Arduino.h>
+#include <math.h>
+
+// -------------------------------------------------------------------
+//                         UTILS - DECLARATIONS
+// -------------------------------------------------------------------
+
+// ---------------- Numeric helpers ----------------
+double clamp(double v, double lo, double hi);
+double rad2deg(double rad);
+double deg2rad(double deg);
+
+extern const float PROTOCOL;
+
+// ---------------- Timing / motion helpers ----------------
+float pvToTicksPerSec(int pvLSB);
+uint32_t estimateTravelTimeMs(uint8_t id, int deltaTicks);
+
+// ---------------- Mapping helpers ----------------
+double mapf(double val, double in_min, double in_max, double out_min, double out_max);
+double constrainf(double val, double min_val, double max_val);
